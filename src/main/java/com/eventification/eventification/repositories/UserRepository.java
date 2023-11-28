4cbce46f46f61684ac63ea7f647d270676f6846b
eliseusalomao <eliseusalomao20@gmail.com>
2023-11-28 15:34:21 -0300

    Cria repository de User

---
+++ src/main/java/com/eventification/eventification/repositories/UserRepository.java
@@ -0,0 +1,13 @@
+package com.eventification.eventification.repositories;
+
+import com.eventification.eventification.models.user.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Integer> {
+
+    Optional<User> findUserByEmail(String email);
+}
