f22dde5eaa5f00252c2cf3306358a96fe017b6d9
eliseusalomao <eliseusalomao20@gmail.com>
2023-11-28 15:33:52 -0300

    Cria service de User

---
+++ src/main/java/com/eventification/eventification/services/UserService.java
@@ -0,0 +1,40 @@
+package com.eventification.eventification.services;
+
+import com.eventification.eventification.exceptions.MinimumPasswordLengthRequired;
+import com.eventification.eventification.exceptions.UserAlreadyExists;
+import com.eventification.eventification.models.user.User;
+import com.eventification.eventification.models.user.UserRole;
+import com.eventification.eventification.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class UserService {
+
+    @Autowired
+    UserRepository repository;
+
+    public User create(String name, String email, String password, String role) throws UserAlreadyExists, MinimumPasswordLengthRequired {
+        Optional<User> userAlreadyExists = repository.findUserByEmail(email);
+
+        if (userAlreadyExists.isPresent()) {
+            throw new UserAlreadyExists();
+        }
+
+        if (password.length() < 6) {
+            throw new MinimumPasswordLengthRequired();
+        }
+
+        User userToBeCreated = new User();
+        userToBeCreated.setName(name);
+        userToBeCreated.setEmail(email);
+        userToBeCreated.setPassword(password);
+        userToBeCreated.setRole(UserRole.valueOf(role));
+
+        repository.save(userToBeCreated);
+
+        return userToBeCreated;
+    }
+}
