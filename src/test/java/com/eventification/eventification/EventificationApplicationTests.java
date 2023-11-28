a4b9e8489ebc7c97087652fd5eedc8e655fdac9f
eliseusalomao <eliseusalomao20@gmail.com>
2023-11-28 11:26:53 -0300

    Initial commit

---
+++ src/test/java/com/eventification/eventification/EventificationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eventification.eventification;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventificationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
