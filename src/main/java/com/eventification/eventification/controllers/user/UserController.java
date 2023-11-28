720ff948ec8c69c4f6094e6939ec7512fd8e16d9
eliseusalomao <eliseusalomao20@gmail.com>
2023-11-28 15:32:54 -0300

    Cria controller de User

---
+++ src/main/java/com/eventification/eventification/controllers/user/UserController.java
@@ -0,0 +1,26 @@
+package com.eventification.eventification.controllers.user;
+
+import com.eventification.eventification.models.user.User;
+import com.eventification.eventification.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/user")
+public class UserController {
+
+    @Autowired
+    UserService service;
+
+    public ResponseEntity<Object> create(User user) throws Exception {
+        try {
+            User createdUser = service.create(user.getName(), user.getEmail(), user.getPassword(), user.getRole().toString());
+            return ResponseEntity.status(HttpStatus.CREATED).body(createdUser);
+        } catch (Exception err) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(err.getMessage());
+        }
+    }
+}
